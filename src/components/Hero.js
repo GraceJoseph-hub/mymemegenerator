89659f743ba1bc05a5406436050f2a883f8c6e7a
Grace <gracecossai@gmail.com>
2023-03-30 10:45:19 +0100

    almost ready meme generator

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -18,26 +18,54 @@ const Hero = (props) => {
     const randomNum = Math.floor(Math.random() * memes.length);
     const getUrl = memes[randomNum].url;
 
-    setImageState((imageState) => {
+    setImageState((prevImageState) => {
       return {
-        ...imageState,
+        ...prevImageState,
         randomImg: getUrl,
+      };
+    });
+  }
+
+  function handleChange(event) {
+    const { name, value, type } = event.target;
+    setImageState((prevImageState) => {
+      return {
+        ...prevImageState,
+        [name]: value,
       };
     });
   }
 
   return (
     <div className="hero-section">
-      <div className="input-fields">
-        <input type="text" placeholder="Shut up" />
-        <input type="text" placeholder="and take my money" />
-      </div>
+      <form className="input-fields">
+        <input
+          type="text"
+          placeholder="Shut up"
+          name="topText"
+          value={imageState.topText}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          placeholder="and take my money"
+          name="bottomText"
+          value={imageState.bottomText}
+          onChange={handleChange}
+        />
+      </form>
       <div className="randomImg-div">
         <div className="second-input" onClick={imageGenerator}>
           <button>Get a new meme image</button>
           <img src={img2} alt="" />
         </div>
-        <img src={imageState.randomImg} alt="" className="random-img" />
+        <div className="text-imgContainer">
+          <img src={imageState.randomImg} alt="" className="random-img" />
+          <div className="text-container">
+            <h2>{imageState.topText}</h2>
+            <h2>{imageState.bottomText}</h2>
+          </div>
+        </div>
       </div>
     </div>
   );
